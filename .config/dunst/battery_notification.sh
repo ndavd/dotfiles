4494b5935a3f94928d3bf37f5a28cd5905d0baf5
ndav1d <ndavidq0@gmail.com>
2021-11-05 15:12:22 +0000

    Fixed battery_notification.sh

--- .config/dunst/battery_notification.sh
+++ .config/dunst/battery_notification.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-pidfile=/tmp/myscript.pid
-trap "rm -f $pidfile; exit 2" QUIT TERM INT KILL
+pidfile=/tmp/notification.pid
+trap 'rm -f /tmp/notification.pid' EXIT TERM QUIT KILL ABRT INT
 if [[ -f $pidfile ]]; then
   printf 'the script is running already: %d\n' "$(cat "$pidfile")" 1>&2
   exit 1
@@ -18,9 +18,14 @@ get_battery_status() {
 
 send_notification() {
   if [ $(get_battery_capacity) -le 15 ] && [ $(get_battery_status) == "Discharging" ]; then
-    notify-send -u critical -i $HOME/.config/dunst/icons/battery_low.png \
-    "Critical Battery Warning ($(get_battery_capacity)%)" "I'm hungry :[" \
-    & mpv $HOME/.config/dunst/bottle.mp3
+    if [[ $notificationpid == '' ]]; then
+      notificationpid=$(dunstify -u critical -i $HOME/.config/dunst/icons/battery_low.png \
+        "Critical Battery Warning ($(get_battery_capacity)%)" "I'm hungry :[" -p)
+    else
+      dunstify -u critical -i $HOME/.config/dunst/icons/battery_low.png \
+        "Critical Battery Warning ($(get_battery_capacity)%)" "I'm hungry :[" -r $notificationpid
+    fi
+    mpv $HOME/.config/dunst/bottle.mp3
   fi
 }
 
