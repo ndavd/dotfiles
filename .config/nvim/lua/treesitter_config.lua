62da31e7870963382d77b3affb138c617edd3b35
Nuno David <email@ndavd.com>
2022-04-10 14:39:46 +0100

    nvim: install all treesitter parsers

--- .config/nvim/lua/treesitter_config.lua
+++ .config/nvim/lua/treesitter_config.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   -- indent = { enable = true },
   autotag = { enable = true },
   highlight = { enable = true },
