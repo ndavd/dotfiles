50226bf2c4a64944d911f93f1cbe9f2c228fa707
Nuno David <ndavidq0@gmail.com>
2022-10-15 13:06:40 +0100

    Fix(nvim): Update deprecated function

--- .config/nvim/lua/lspconfig_config.lua
+++ .config/nvim/lua/lspconfig_config.lua
@@ -25,7 +25,7 @@ vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
 -- }
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 
 -- Make root_dir the current directory ( not recommended )
