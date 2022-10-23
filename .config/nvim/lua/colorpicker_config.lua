502c0ba33f210f5d4b580e44c03d0641f4554c6d
Nuno David <ndavidq0@gmail.com>
2022-10-23 17:20:24 +0100

    Fix(nvim): Add missing files

---
+++ .config/nvim/lua/colorpicker_config.lua
@@ -0,0 +1,12 @@
+require("color-picker").setup({ -- for changing icons & mappings
+  -- ["icons"] = { "ﱢ", "" },
+  -- ["icons"] = { "ﮊ", "" },
+  -- ["icons"] = { "", "ﰕ" },
+  -- ["icons"] = { "", "" },
+  -- ["icons"] = { "", "" },
+  ["icons"] = { "ﱢ", "" },
+  ["keymap"] = { -- mapping example:
+    ["U"] = "<Plug>Slider5Decrease",
+    ["O"] = "<Plug>Slider5Increase",
+  },
+})
