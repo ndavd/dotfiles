d491cfa77e482e121e5cf9456b7d04c930bac7c9
Nuno David <ndavidq0@gmail.com>
2023-03-05 18:46:59 +0000

    Feat: Dual screen setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,4 +10,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # polybar primary &
 
 # For bspwm
-polybar bspwm_version &
+polybar bspwm_version_primary &
+
+if [[ $(xrandr -q | grep 'HDMI-0 connected') ]]; then
+  polybar bspwm_version_secondary &
+fi
