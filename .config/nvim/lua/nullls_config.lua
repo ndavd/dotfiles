b4778dc3302679427e0fe378896e68c22e11343b
Nuno David <ndavidq0@gmail.com>
2023-04-22 00:00:40 +0100

    Feat(nvim): Add prettier formatter

--- .config/nvim/lua/nullls_config.lua
+++ .config/nvim/lua/nullls_config.lua
@@ -15,27 +15,15 @@ local extra_args = {
 }
 
 local sources = {
-  --[[ Let's just use eslint as formatter and if prettier is needed just setup `eslint-plugin-prettier` ]]
-  -- formatting.prettier.with({
-  --   filetypes = {
-  --     'javascript',
-  --     'javascriptreact',
-  --     'typescript',
-  --     'typescriptreact',
-  --     'vue',
-  --     'css',
-  --     'scss',
-  --     'less',
-  --     'html',
-  --     'json',
-  --     'jsonc',
-  --     'yaml',
-  --     'markdown.mdx',
-  --     'graphql',
-  --     'handlebars',
-  --   },
-  -- }),
   formatting.eslint,
+  formatting.prettier.with({
+    runtime_condition = function(params)
+      local utils = require('null-ls.utils')
+      -- use whatever root markers you want to check - these are the defaults
+      local root = utils.root_pattern('.null-ls-root', 'Makefile', '.git')(params.bufname)
+      return root and utils.path.exists(utils.path.join(root, '.prettierrc'))
+    end,
+  }),
   formatting.deno_fmt.with({
     filetypes = { 'markdown' },
   }),
