979c0a7879f9b406abc7ad5a51afa55416cf4176
Nuno David <ndavidq0@gmail.com>
2023-08-26 12:17:33 +0100

    Fix(polybar): Spawn external monitor bar first

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,8 +10,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # polybar primary &
 
 # For bspwm
-polybar bspwm_version_primary &
 
 if [[ $(xrandr -q | grep 'HDMI-0 connected') ]]; then
   polybar bspwm_version_secondary &
+  polybar bspwm_version_primary &
+else
+  polybar bspwm_version_primary &
 fi
