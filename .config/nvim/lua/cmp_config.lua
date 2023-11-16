b448ebe21a299b9b44e477ea25826c49a337696d
Nuno David <ndavidq0@gmail.com>
2023-11-16 11:41:50 +0000

    feat(cmp): safely select entries with <cr>

--- .config/nvim/lua/cmp_config.lua
+++ .config/nvim/lua/cmp_config.lua
@@ -9,7 +9,17 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm({ select = true }),
+      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    }),
     ['<C-e>'] = cmp.mapping.close(),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
