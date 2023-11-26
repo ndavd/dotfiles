cf0fb891875846353569332ef8ba079797e07283
Nuno David <ndavidq0@gmail.com>
2023-11-26 20:56:09 +0000

    feat(nvim): finish full migration to lua

---
+++ .config/nvim/lua/init.lua
@@ -0,0 +1,83 @@
+-- Define mapleader
+vim.g.mapleader = ' '
+
+-- 256 color support
+vim.o.termguicolors = true
+
+-- Settings
+vim.o.wrap = false
+vim.o.clipboard = 'unnamedplus'
+vim.o.showmode = false
+vim.o.errorbells = false
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.cursorline = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.hlsearch = false
+vim.o.list = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
+vim.o.pumblend = 15
+vim.o.textwidth = 80
+vim.o.completeopt = 'menuone,noselect'
+vim.o.shortmess = vim.o.shortmess .. 'c'
+vim.o.inccommand = 'split'
+vim.o.foldmethod = 'marker'
+vim.o.formatexpr = 'v:lua.require\'conform_config\'.formatexpr()'
+
+-- Title
+vim.o.title = true
+vim.o.titlestring = '%t %m - NVIM'
+
+-- Change guicursor
+vim.o.guicursor = 'i-r:hor20-Cursor'
+
+-- Mouse
+vim.o.mouse = 'a'
+vim.o.mousemodel = 'extend'
+vim.o.selection = 'inclusive'
+
+-- Fix highlight errors
+vim.g.vimsyn_noerror = 1
+
+-- Highlight when yanking
+local highlight_on_yank =
+  vim.api.nvim_create_augroup('highlight_on_yank', { clear = true })
+vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
+  pattern = '*',
+  group = highlight_on_yank,
+  callback = require('vim.highlight').on_yank,
+})
+
+-- Rasi (rofi theme file)
+local rasi_custom = vim.api.nvim_create_augroup('rasi_custom', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = '*.rasi',
+  group = rasi_custom,
+  callback = function()
+    vim.bo.syntax = 'css'
+  end,
+})
+
+-- Markdown
+local markdown_custom =
+  vim.api.nvim_create_augroup('markdown_custom', { clear = true })
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = 'markdown',
+  group = markdown_custom,
+  callback = function()
+    vim.wo.conceallevel = 2
+  end,
+})
+
+require('plugins')
+require('colors')
+require('keymaps')
+require('stl')
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -32,3 +32,43 @@ keymap('n', '<leader>tp', '<cmd>tabp<CR>')
 keymap('n', '<leader>p', '<cmd>setlocal spell spelllang=en_us<CR>')
 keymap('n', '<leader>pt', '<cmd>setlocal spell spelllang=pt_pt<CR>')
 keymap('n', '<S-p>', '<cmd>set nospell<CR>')
+
+-- Execute neovim custom settings from current file contents, e.g.
+-- :NVIM_CUSTOM: echo "Hello, World!"
+keymap('n', '<leader>cc', function()
+  local keyword = 'NVIM_CUSTOM'
+  local keystr = ':' .. keyword .. ':'
+  local commands = vim
+    .iter(vim.fn.readfile(tostring(vim.fn.expand('%:p'))))
+    :map(function(line)
+      local index = string.find(line, keystr)
+      if index ~= nil then
+        return string.sub(line, index + #keystr)
+      end
+    end)
+    :totable()
+  if #commands == 0 then
+    print('Couldn\'t find any custom NVIM settings...')
+    return
+  end
+  local success = pcall(vim.api.nvim_exec2, table.concat(commands, '\n'), {})
+  if success then
+    print('Loaded ' .. #commands .. 'l of custom NVIM settings')
+  end
+end)
+
+-- Cycle listchars
+if vim.o.list then
+  local listchar_index = 1
+  local listchar_options = {
+    'tab:_\\ ,conceal:┊,nbsp:⍽,extends:>,precedes:<,trail:·,eol:﬋',
+    'tab:_\\ ,conceal:┊,nbsp:⍽,extends:>,precedes:<,trail:·',
+    '',
+  }
+  local cycle_listchars = function()
+    vim.o.listchars = listchar_options[listchar_index]
+    listchar_index = (listchar_index % #listchar_options) + 1
+  end
+  keymap('n', '<leader><leader>cl', cycle_listchars)
+  cycle_listchars()
+end
