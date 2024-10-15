4407ab0273dd7d1d80860cf2d92595667aaf14b5
Nuno David <ndavidq0@gmail.com>
2024-10-15 14:52:31 +0100

    Revert "feat(nvim): remove eslint from conform"
    This reverts commit 093c68d46ddfc5bdec7eb25955ba5968438f8f92.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,8 +1,51 @@
 local out = {}
 
 local conform = require('conform')
+local conform_utils = require('conform.util')
 
-local prettierd = { 'prettierd', 'prettier' }
+local eslint_files = {
+  '.eslintrc.js',
+  '.eslintrc.cjs',
+  '.eslintrc.yaml',
+  '.eslintrc.yml',
+  '.eslintrc.json',
+}
+local prettier_files = {
+  '.prettierrc',
+  '.prettierrc.json',
+  '.prettierrc.yml',
+  '.prettierrc.yaml',
+  '.prettierrc.json5',
+  '.prettierrc.js',
+  '.prettierrc.cjs',
+  '.prettierrc.toml',
+  'prettier.config.js',
+  'prettier.config.cjs',
+}
+
+---@param files string[]
+local with_package_json = function(files)
+  local arr = {}
+  for i, f in ipairs(files) do
+    arr[i] = f
+  end
+  table.insert(arr, 'package.json')
+  return arr
+end
+
+---@param path string
+---@param files string|string[]
+---@return string|nil
+local get_path_to_file = function(path, files)
+  local found = vim.fs.find(files, { upward = true, path = path })
+  if #found == 0 then
+    return nil
+  end
+  return found[1]
+end
+
+local eslintd = { 'eslint_d' }
+local eslintd_prettier = { 'eslint_d', { 'prettierd', 'prettier' } }
 local denofmt = { 'deno_fmt' }
 local stylua = { 'stylua' }
 local gofmt = { 'gofmt' }
@@ -12,12 +55,32 @@ local shellharden = { 'shellharden' }
 local yamlfmt = { 'yamlfmt' }
 local forgefmt = { 'forge_fmt' }
 
+local js_ts_x = function()
+  local root = vim.lsp.buf.list_workspace_folders()[1]
+
+  if get_path_to_file(root, prettier_files) then
+    return eslintd_prettier
+  end
+
+  -- Check for package.json config entry
+  local package_json_path = get_path_to_file(root, 'package.json')
+  if package_json_path then
+    local package_json =
+      vim.json.decode(table.concat(vim.fn.readfile(package_json_path)))
+    if package_json['prettier'] ~= nil then
+      return eslintd_prettier
+    end
+  end
+
+  return eslintd
+end
+
 conform.setup({
   formatters_by_ft = {
-    javascript = prettierd,
-    typescript = prettierd,
-    javascriptreact = prettierd,
-    typescriptreact = prettierd,
+    javascript = js_ts_x,
+    typescript = js_ts_x,
+    javascriptreact = js_ts_x,
+    typescriptreact = js_ts_x,
     markdown = denofmt,
     lua = stylua,
     go = gofmt,
@@ -28,12 +91,24 @@ conform.setup({
     yaml = yamlfmt,
     solidity = forgefmt,
   },
+
+  formatters = {
+    eslint_d = {
+      cwd = conform_utils.root_file(with_package_json(eslint_files)),
+    },
+    prettier = {
+      cwd = conform_utils.root_file(with_package_json(prettier_files)),
+    },
+    prettierd = {
+      cwd = conform_utils.root_file(with_package_json(prettier_files)),
+    },
+  },
   notify_on_error = true,
   -- log_level = vim.log.levels.DEBUG,
 })
 
 Formatexpr = function()
-  return conform.formatexpr({ timeout_ms = 10000, lsp_fallback = true })
+  return conform.formatexpr({ timeout_ms = 10000 })
 end
 
 vim.o.formatexpr = 'v:lua.Formatexpr()'
