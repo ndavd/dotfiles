61aec81fed754eb9cc31d60a02baa6eb761442ff
Nuno David <ndavidq0@gmail.com>
2026-02-26 12:45:59 +0000

    fix(nvim): update treesitter config to new api

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,23 @@
--- Folds
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.o.foldtext = ''
-vim.o.foldlevelstart = 99
-
-require('nvim-treesitter.configs').setup({
-  ensure_installed = 'all',
-  -- indent = { enable = true },
-  autotag = { enable = true },
-  highlight = {
-    enable = true,
-    disable = { 'markdown' },
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
-    },
-  },
+require('nvim-treesitter').setup({
+  install_dir = vim.fn.stdpath('data') .. '/site',
 })
 
 require('ts_context_commentstring').setup({
   enable_autocmd = false,
+})
+
+-- Folds
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldtext = ''
+vim.o.foldlevelstart = 99
+
+-- Indents
+vim.o.indentexpr = 'v:lua.require\'nvim-treesitter\'.indentexpr()'
+
+-- Highlights
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
 })
