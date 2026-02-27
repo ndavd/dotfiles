c14a6787d7ee07834b1eb65b33875ccd1b9e61b4
Nuno David <ndavidq0@gmail.com>
2026-02-27 00:14:29 +0000

    feat(nvim): replace nvim-cmp with blink.cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,45 +1,40 @@
-local cmp = require('cmp')
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn['vsnip#anonymous'](args.body)
-    end,
+require('blink.cmp').setup({
+  keymap = {
+    preset = 'enter',
+    ['<C-u>'] = {
+      function(cmp)
+        cmp.scroll_documentation_up(4)
+      end,
+    },
+    ['<C-d>'] = {
+      function(cmp)
+        cmp.scroll_documentation_down(4)
+      end,
+    },
   },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-n>'] = cmp.mapping.select_next_item({
-      behavior = cmp.SelectBehavior.Select,
-    }),
-    ['<C-p>'] = cmp.mapping.select_prev_item({
-      behavior = cmp.SelectBehavior.Select,
-    }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-    { name = 'path' },
-    { name = 'treesitter' },
-    { name = 'spell' },
-  },
-  window = {
-    completion = cmp.config.window.bordered({
+  completion = {
+    list = {
+      selection = {
+        preselect = true,
+        auto_insert = false,
+      },
+    },
+    documentation = {
+      auto_show = true,
+      window = {
+        winblend = vim.o.pumblend,
+      },
+    },
+    menu = {
       border = 'solid',
-    }),
-    documentation = cmp.config.window.bordered(),
+      winblend = vim.o.pumblend,
+      max_height = 99,
+    },
+  },
+  signature = {
+    enabled = true,
+    window = {
+      winblend = vim.o.pumblend,
+    },
   },
 })
-
--- vsnip
-local jumpable_n = vim.fn['vsnip#jumpable'](1)
-local jumpable_p = vim.fn['vsnip#jumpable'](-1)
-vim.keymap.set({ 'i', 's' }, '<C-k>', function()
-  return jumpable_n and '<Plug>(vsnip-jump-next)' or '<C-k>'
-end, { expr = true })
-vim.keymap.set({ 'i', 's' }, '<C-j>', function()
-  return jumpable_p and '<Plug>(vsnip-jump-prev)' or '<C-j>'
-end, { expr = true })
