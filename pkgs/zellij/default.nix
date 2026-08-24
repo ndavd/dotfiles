0fdf95d7bd990237ec242a3a97cc196811e257cc
Nuno David <ndavidq0@gmail.com>
2026-08-24 23:34:57 +0100

    feat: make copy command customizable on zellij pkg

--- pkgs/zellij/default.nix
+++ pkgs/zellij/default.nix
@@ -1,9 +1,12 @@
-{ pkgs }:
+{
+  pkgs,
+  copy-command ? "wl-copy",
+}:
 let
   zellij-cb = pkgs.zellijPlugins.cb;
 
   configDir = pkgs.linkFarm "zellij-config" {
-    "config.kdl" = ./config/config.kdl;
+    "config.kdl" = pkgs.replaceVars ./config/config.kdl { inherit copy-command; };
     "layouts/default.kdl" = pkgs.replaceVars ./config/layouts/default.kdl { inherit zellij-cb; };
     "layouts/project.kdl" = pkgs.replaceVars ./config/layouts/project.kdl { inherit zellij-cb; };
   };
