4f27352fd5255231d78556ecb0aef71038472e03
Nuno David <ndavidq0@gmail.com>
2026-09-05 17:34:54 +0100

    feat(nvim): align MiniGit blame output with window

--- pkgs/nvim/config/lua/plugins/mini.lua
+++ pkgs/nvim/config/lua/plugins/mini.lua
@@ -300,7 +300,21 @@ local custom_conf = {
       vim.b[data.buf].minigit_summary_string = vim.b[data.buf].minigit_summary.head_name
     end
 
+    local align_blame = function(au_data)
+      if au_data.data.git_subcommand ~= 'blame' then
+        return
+      end
+      -- Align blame output with source
+      local win_src = au_data.data.win_source
+      vim.wo.wrap = false
+      vim.fn.winrestview({ topline = vim.fn.line('w0', win_src) })
+      vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
+      -- Bind both windows so that they scroll together
+      vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
+    end
+
     aug.add('User', { pattern = 'MiniGitUpdated', callback = format_summary_string })
+    aug.add('User', { pattern = 'MiniGitCommandSplit', callback = align_blame })
 
     return {
       command = {
